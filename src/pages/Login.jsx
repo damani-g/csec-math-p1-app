b855239eb0e82972bdec3705b52c406e0c754295
Damani G <damani.garcia6221@gmail.com>
2025-06-02 23:43:28 -0400

    Google Sign-In

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -51,7 +51,13 @@ export default function Login() {
       <div className="login-container">
         <h2>Sign In</h2>
         <div className="google-login-btn">
-          <button onClick={handleGoogleLogin}>Sign in with Google</button>
+          <button onClick={handleGoogleLogin}>
+            <img 
+              src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" 
+              alt="Google logo" 
+            />
+            <span>Sign in with Google</span>
+          </button>
         </div>
         
 
